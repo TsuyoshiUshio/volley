c5e05ce3e98fdd42efa6eed041497be10e669c06
Tsuyoshi Ushio <ushio@simplearchitect.com>
2019-12-13 21:17:07 -0800

    Adding test case for property file is already exists.

--- pkg/controller/property_controller_test.go
+++ pkg/controller/property_controller_test.go
@@ -53,6 +53,46 @@ func TestJUpdateJMeterConfig_NormalCase(t *testing.T) {
 	assert.Equal(t, actualResponseWriter.WrittenHeaders[0], 200)
 }
 
+func TestJUpdateJMeterConfig_Conifg_Already_Exists(t *testing.T) {
+	inputJson := "{\"remote_host_ips\": [\"10.0.0.1\",\"10.0.0.2\"]}"
+	expectedJMeterPropertyFilePath := filepath.Join("foo", "bar", "jmeter.properties")
+
+	actualResponseWriter := &MyResponseWriter{
+		WrittenResponse: strings.Builder{},
+		WrittenHeaders:  []int{},
+	}
+	context := gin.Context{
+		Request: &http.Request{
+			Body: ioutil.NopCloser(bytes.NewReader([]byte(inputJson))),
+		},
+		Writer: actualResponseWriter,
+	}
+
+	monkey.Patch(helper.GetJMeterPropertyFilePath, func() string {
+		return expectedJMeterPropertyFilePath
+	})
+	monkey.Patch(os.Stat, func(file string) (os.FileInfo, error) {
+		assert.Equal(t, model.JMeterPropertyFile, file)
+		return nil, nil
+	})
+	monkey.Patch(os.Remove, func(file string) error {
+		assert.Equal(t, model.JMeterPropertyFile, file)
+		return nil
+	})
+
+	var p *model.JMeterProperty
+
+	monkey.PatchInstanceMethod(reflect.TypeOf(p), "GenerateModifiedProperty", func(p *model.JMeterProperty, source, destination string) error {
+		assert.Equal(t, expectedJMeterPropertyFilePath, source)
+		return nil
+	})
+	defer monkey.UnpatchAll()
+	UpdateJMeterConfig(&context)
+
+	assert.Equal(t, "{\"remote_host_ips\":[\"10.0.0.1\",\"10.0.0.2\"]}\n", actualResponseWriter.WrittenResponse.String())
+	assert.Equal(t, actualResponseWriter.WrittenHeaders[0], 200)
+}
+
 type MyResponseWriter struct {
 	WrittenResponse strings.Builder
 	WrittenHeaders  []int
